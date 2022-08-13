1a8df04cb82fbc3c6e0b4dc9d871d5ac5b08725b
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-08-13 03:26:07 -0300

    Convertendo temperatura de Celsius para Fahrenheit

--- converters-ios/Views/ContentView.swift
+++ converters-ios/Views/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
+    @State private var temperature: String = ""
+    @State private var temperatureConverted: Double = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            GeometryReader { view in
+                VStack {
+                        
+                    TextField("Teperatura en Celsius", text: $temperature)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                        
+                        .padding(20)
+                        
+                    
+                    Button(action: {
+                        temperatureConverted = ((Double(temperature) ?? 0.0) * 9/5) + 32
+                    }) {
+                        Text("Pacotes")
+                            .font(.custom("Avenir Medium", size: self.horizontalSizeClass == .compact ? 17 : 24))
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    }
+                    .frame(width: self.horizontalSizeClass == .compact ? 100 : 150,
+                           height: self.horizontalSizeClass == .compact ? 50 : 75,
+                           alignment: .center)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.orange, lineWidth: 10))
+                    .background(.orange)
+                    
+                    Text(String(temperatureConverted))
+                        .padding(.top, 20)
+                    
+                    
+                }
+                .padding(.top, 100)
+                .padding(.leading, 15)
+                
+            }.edgesIgnoringSafeArea(.all)
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
