b276479945ff4f19be5eb558f5d52227036b1b5a
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-08-13 01:57:21 -0300

    Estrutura do projeto

---
+++ converters-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  converters-ios
+//
+//  Created by Rodrigo Amora on 13/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ converters-ios/converters_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  converters_iosApp.swift
+//  converters-ios
+//
+//  Created by Rodrigo Amora on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct converters_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
