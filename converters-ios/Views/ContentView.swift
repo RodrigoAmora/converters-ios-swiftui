2827587477b69403bf4d098fbb78cac46107bbb0
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-09-26 02:09:31 -0300

    Ajustando o layout

--- converters-ios/Views/ContentView.swift
+++ converters-ios/Views/ContentView.swift
@@ -13,21 +13,41 @@ struct ContentView: View {
     
     @State private var temperature: String = ""
     @State private var temperatureConverted: Double = 0.0
+    @State var selectedOptionIndex = 0
     
     var body: some View {
         let options: [String] = ["Celsius para Fahrenheit",
                                  "Fahrenheit para Celsius"]
         
-        NavigationView {
-            GeometryReader { view in
-                VStack {
-                    ConverterComponent(placeholderTextField: "Temperatura em Celsius", options: options, option: OptionsEnum.temperature)
-                }
-                .padding(.top, 100)
+        VStack{
+            TextField("Digite a temepratua", text: $temperature)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.decimalPad)
+                .padding(.trailing, 15)
                 .padding(.leading, 15)
-                
-            }.edgesIgnoringSafeArea(.all)
-        }.navigationViewStyle(StackNavigationViewStyle())
+            
+            Picker("Escolha uma opção", selection: $selectedOptionIndex, content: {
+                ForEach(0..<options.count, content: { index in // <2>
+                    Text(options[index]) // <3>
+                })
+            })
+            
+            Button(action: {}) {
+                Text("Converter")
+                    .font(.custom("Avenir Medium", size: self.horizontalSizeClass == .compact ? 17 : 24))
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+            }
+            .frame(width: self.horizontalSizeClass == .compact ? 100 : 150,
+                   height: self.horizontalSizeClass == .compact ? 50 : 75,
+                   alignment: .center)
+            .overlay(RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.orange, lineWidth: 10))
+            .background(.orange)
+            .padding(.top, 7)
+            
+            Text(String(temperatureConverted))
+                .padding(.top, 20)
+        }
     }
 }
 
